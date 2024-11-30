ce31707417b10a32e98cf54e9ecf1bd3c8393990
dengagaga <134398955+dengagaga@users.noreply.github.com>
2024-11-30 13:07:10 +0300

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,20 @@
+// SCROLL
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.4] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+  
+  
